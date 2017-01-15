0ad50071810ffe9ffbe5c4075a051d15cea02b20
murch <ryan.c.murchie@gmail.com>
2017-01-15 00:48:47 -0500

    initial commit header comments func skeleton

---
+++ categorizeAppend.R
@@ -0,0 +1,22 @@
+#
+# CATEGORIZE AND APPEND FUNCTION
+#
+#
+# Function to categorize a subset of columns in a data.frame while returning a new data.frame with new columns appended (adjacent to categorized columns) 
+#
+# - passes data.frame containing dataset, indices of the columns to be categorized, and category hash table
+# - creates one new column for each column categorized
+# - returns data.frame with new columns insert after categorized columns
+#
+# arguments:
+#   input - value to be categorized
+#   categoryHash - hash table containing categories to be mapped
+#
+# return:
+#   output - hash table converted value (typically as character)
+
+categorizeAppend <- function(dataSet, indices, categoryHash)
+{
+  
+  
+}
