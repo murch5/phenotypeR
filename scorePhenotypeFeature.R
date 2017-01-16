42aa4c224a30691c611f5108c950746be3c72aae
murch <ryan.c.murchie@gmail.com>
2017-01-16 16:50:42 -0500

    design func test data

--- scorePhenotypeFeature.R
+++ scorePhenotypeFeature.R
@@ -12,9 +12,42 @@
 # arguments:
 #   input - data.frame containing subset of phenotypic data to be analyzed
 #   recodeValues - data.frame containing phenotype ID with associated values (values according to severity of phenotypic classification)
-#   locationWeights - data.frame containing location ID and associated weightings (according to regions associated with greater disease severity)
+#   featureMapping - data.frame containing feature  and associated weightings (according to regions associated with greater disease severity)
 #
 # return:
 #   output - data.frame containing new score values
 
-scorePhenotypeByFeature <- function(input,)
+scorePhenotypeByFeature <- function(input,featureMapping)
+{
+  
+  scoresFeature <- t(lapply(featureMapping, function(x){
+    
+    temp <- unique(input[,x[["columnID"]]])
+    
+    unweightedScore <- x[["map"]][which(x[["map"]][,"type"]==temp),"value"]
+    if(length(unweightedScore)<1){unweightedScore = 0}
+    return(unweightedScore)
+  }))
+  
+  scoresFeature <- as.data.frame(scoresFeature)
+  
+  print(featureMapping)
+ 
+  
+  return(scoresFeature)
+}
+
+testData <- read.csv("test.csv",stringsAsFactors=FALSE)
+testData2 <- testData[c(2:8), c("Luminal", "EIM")]
+
+luminalMap <- data.frame(type = c("","Inflammatory"), value = c(0,1),stringsAsFactors = FALSE)
+EIM <- data.frame(type = c("","IDDM"), value = c(0,1),stringsAsFactors = FALSE)
+
+l <- list("Luminal",luminalMap)
+names(l) <- c("columnID","map")
+e <- list("EIM",EIM)
+names(e) <- c("columnID","map")
+
+featureMapping <- list(l,e)
+
+gg <- scorePhenotypeByFeature(testData2,featureMapping)
