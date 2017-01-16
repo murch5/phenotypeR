f57745088afc013209fc19711035af778775c0ce
murch <ryan.c.murchie@gmail.com>
2017-01-15 20:20:18 -0500

    initial commit comment header

---
+++ scorePhenotype.R
@@ -0,0 +1,25 @@
+#
+# SCORE PHENOTYPE FUNCTION
+#
+#
+# Function used to score individual phenotypes according to passed weightings
+#
+# - passes data.frame containing subset of phenotypic information to be score, data.frame containing columns names and associated scoring functions for each)
+#   and data.frame containing scoring groupings to be applied to calculated different total scores
+# -creates various scoring columns according to arguments passed
+# - returns data.frame with new score columns
+#
+# arguments:
+#   dataSet - data.frame containing subset of phenotypic data to be analyzed
+#   
+#   
+#
+# return:
+#   output - data.frame containing new score values
+
+
+scorePhenotype <- function(input,scoringFuncMapping,scoringGrouping)
+{
+  
+  return(output)
+}
