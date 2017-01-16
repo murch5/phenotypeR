e832dd104e2a41dda1cb11d65da3d7eb634059ae
murch <ryan.c.murchie@gmail.com>
2017-01-16 02:13:01 -0500

    using dplyr to transverse phenotype data to collate scores by hierarchy

--- scoreSummaryPhenotype.R
+++ scoreSummaryPhenotype.R
@@ -2,10 +2,10 @@
 # SCORE SUMMARY - PHENOTYPE FUNCTION
 #
 #
-# Function used to compile scored site-specific phenotypes compiled by ID, Encounter Type, Date, and Ix
+# Function used to score and compile location site-specific phenotypes compiled by ID, Encounter Type, Date, and Ix
 # Adds covariates to aggregrate score values
 #
-# - passes data.frame containing subset of phenotypic information to be score, data.frame containing values to recoded phenotypic categories
+# - passes data.frame containing phenotypic , data.frame containing values to recoded phenotypic categories
 #   and data.frame containing weighting characteristic of each location
 # -creates various scoring columns according to arguments passed
 # - returns data.frame with new score columns
@@ -18,7 +18,17 @@
 # return:
 #   output - data.frame containing new score values
 
+library(dplyr)
+library(plyr)
+
+source("scorePhenotype.R")
+
 scoreSummaryPhenotype <- function(input,recodeValues, locationWeights)
 {
   
+  d <-
+    input %>% group_by(MuiseLabID, Tbl_Encounter.Timing, Date, Ix) %>% select(Site, Involvement) %>% do(scorePhenotypeByLocation(., recodeValues, locationWeights))
+  return(d)
 }
+
+ddt <- scoreSummaryPhenotype(testData,data.frame(c("Not Involved", "Macroscopic Disease"), c(0, 3)), data.frame(c("Left Colon", "Perianal"), c(1, 3)))
