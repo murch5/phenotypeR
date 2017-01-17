e74c78a616e50fefe10ce2d55291c10f38ea8ef8
murch <ryan.c.murchie@gmail.com>
2017-01-17 00:16:33 -0500

    added coercion to temp to ensure character

--- scorePhenotypeFeature.R
+++ scorePhenotypeFeature.R
@@ -20,21 +20,18 @@
 scorePhenotypeByFeature <- function(input,featureMapping)
 {
   
-  scoresFeature <- (lapply(featureMapping, function(x){
+  scoresFeature <- lapply(featureMapping, function(x){
     
     temp <- unique(input[,x[["columnID"]]])
-    
-    print(x[["columnID"]])
-    unweightedScore <- x[["map"]][which(x[["map"]][,"type"]==temp),"value"]
+    unweightedScore <- x[["map"]][which(x[["map"]][,"type"]==as.character(temp)),"value"]
     if(length(unweightedScore)<1){unweightedScore = 0}
     
     unweightedScore <- as.data.frame(unweightedScore)
     colnames(unweightedScore) <- paste(x[["columnID"]],"SumScore",sep=" ")
     return(unweightedScore)
-  }))
+  })
   
   scoresFeature <- as.data.frame(scoresFeature)
-
   return(scoresFeature)
 }
 
