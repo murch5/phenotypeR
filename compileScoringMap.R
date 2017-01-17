2f51a520163216604a13c652941ab29b33b235fb
murch <ryan.c.murchie@gmail.com>
2017-01-17 17:00:06 -0500

    initial commit comment header func skeleton

---
+++ compileScoringMap.R
@@ -0,0 +1,19 @@
+#
+# COMPILE SCORING MAP FUNC
+#
+#
+# Function used to compile scoring map for phenotypic features from file
+# 
+#
+# - passes character containing fileName containing encodings
+#
+# -creates various scoring columns according to arguments passed
+# - returns data.frame with new score columns
+#
+# arguments:
+#   input - data.frame containing subset of phenotypic data to be analyzed
+#   recodeValues - data.frame containing phenotype ID with associated values (values according to severity of phenotypic classification)
+#   locationWeights - data.frame containing location ID and associated weightings (according to regions associated with greater disease severity)
+#
+# return:
+#   output - data.frame containing new score values
