003e2b365047498e8a514a88da00320b754063ec
murch <ryan.c.murchie@gmail.com>
2017-01-20 15:39:56 -0500

    initial commit create classifyMutate func based on categorizeMutate func parse comparison operators from string test functions

---
+++ classifyMutate.R
@@ -0,0 +1,81 @@
+#
+# CATEGORIZE AND MUTATE FUNCTION
+#
+#
+# Function to categorize a subset of columns in a data.frame while returning a new data.frame with new columns appended (adjacent to categorized columns) 
+#
+# - passes data.frame containing dataset, indices of the columns to be categorized, and category hash table
+# - creates one new column for each column categorized
+# - returns data.frame with new columns insert after categorized columns
+#
+# arguments:
+#   dataSet - data.frame containing columns to be categorized
+#   indices - vector of columns indices to be categorized
+#   categoryHash - hash table containing values to be remapped
+#
+# return:
+#   output - data.frame containg dataSet with new category columns appended adjacent to original columns
+
+source("categorize.R")  #include source for categorize function
+library(dplyr)
+
+
+evaluateCriteria <- function(dataSubset, criteria, classifyHash)
+{
+ 
+  dataEvaluated <- data.frame()
+ 
+  
+  r <- lapply(criteria,function(x){
+    
+    d <- apply(dataSubset,1,function(y){
+
+       evalExp <- c("y[\"",(x[,1]),"\"]",x[,2],x[,3])
+       evalExp <- paste(evalExp,collapse="")
+       
+       if(eval(parse(text=evalExp)))
+       {
+         return(TRUE)
+       } else
+       {
+         return(FALSE)
+       }
+      
+    })
+    
+    classifiedData <- as.data.frame(d)
+    name <- paste(x,collapse="")
+    colnames(classifiedData) <- name
+    return(classifiedData)
+    
+  })
+  
+  r <- as.data.frame(r,stringsAsFactors=FALSE)
+  dataSubset <- cbind(dataSubset,r)
+  return(dataSubset)
+  
+}
+
+classifyMutate <- function(dataSet, filters, columns, criteria, classifyHash)
+{
+
+  d <-
+    dataSet %>%
+    filter_(.,filters) %>%
+    select_(.,columns) %>%
+    do(evaluateCriteria(.,criteria,classifyHash)) 
+
+  return(d)
+  
+}
+
+test <- read.csv("test2.csv", stringsAsFactors = FALSE)
+categoryHashtest <-
+  data.frame(c(1, 2, 16984), c("Test1", "Test2", "Test2"), stringsAsFactors = FALSE)
+
+
+crit <- data.frame("paternalID",">","1", stringsAsFactors=FALSE)
+crit2 <- data.frame("paternalID","<","1", stringsAsFactors=FALSE)
+critList <- list(crit, crit2)
+
+test2 <- classifyMutate(test[,-1],as.character("sexID>1"),"paternalID",critList, categoryHashtest)
