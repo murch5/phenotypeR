0104395e983a238b5348f3493c4858f3884c0d62
murch <ryan.c.murchie@gmail.com>
2018-04-25 10:49:58 -0400

    fixed pheno scoring func by location to determine groupings for regional involvement determine disease inv groups add logging suppress pkg startup messages

--- score_phenotype_by_location.R
+++ score_phenotype_by_location.R
@@ -17,19 +17,103 @@
 # return:
 #   output - data.frame containing new score values
 
-library(dplyr)
-library(plyr)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(plyr))
+
+determine_involvement <-
+  function(input,
+           subset,
+           operation,
+           condition = NULL,
+           val = NULL) {
+    set <- unlist(subset)
+    subset_data <-
+      input[which(input[, "Site"] %in% set, arr.ind = TRUE),]
+    
+    return_val <- 0
+    
+    if (all(is.na(subset_data[, "Unweighted"])))
+    {
+      return_val <- 0
+      
+    }
+    else
+    {
+      if (operation == "all")
+      {
+        subset_fail <-
+          subset_data[which(subset_data[, "Unweighted"] == 0), "Unweighted"]
+        if (length(subset_fail > 0))
+        {
+          return_val <- 2
+        }
+      }
+      else if (operation == "any")
+      {
+        if (sum(subset_data[, "Unweighted"], na.rm = TRUE) > 0)
+        {
+          return_val <- 2
+        }
+        else
+        {
+          return_val <- 1
+        }
+      }
+      else if (operation == "sum")
+      {
+        if (condition == ">")
+        {
+         
+          if (length(which(subset_data[, "Unweighted"] > 0)) > as.integer(val))
+          {
+            return_val <- 2
+          }
+          else
+          {
+            return_val <- 1
+          }
+        }
+        
+      }
+      
+    }
+    
+    return(return_val)
+  }
+
+
+loc_groups <-
+  read.table(
+    "/home/murch/DATA/project/pheno_geno_correlation/raw/config/location_groups_inv",
+    sep = ";",
+    header = TRUE
+  )
+
+make_location_groupings <- function(data)
+{
+  output <- apply(loc_groups, 1, function(y) {
+    subset_region <- strsplit(y["group"], split = ",")
+    val <-
+      determine_involvement(data, subset_region, y["operation"], y["condition"], y["val"])
+    
+    return(val)
+  })
+  
+  location_group_df <-
+    data.frame(inv = loc_groups[, "loc"],
+               inv_code = output,
+               stringsAsFactors = FALSE)
+  
+  return(location_group_df)
+}
+
 
 scorePhenotypeByLocation <-
   function(input, recode_vals, weights_location)
   {
-    
-
     score <- apply(input, 1, function(x) {
-      
-      
-      
-      score_recoded <- as.numeric(recode_vals[which(recode_vals[, 1] == x["Involvement"]), 2])
+      score_recoded <-
+        as.numeric(recode_vals[which(recode_vals[, 1] == x["Involvement"]), 2])
       
       score_weighted <-
         score_recoded * as.numeric((weights_location[which(weights_location[, 1] == x["Site"]), 2]))
@@ -41,7 +125,13 @@ scorePhenotypeByLocation <-
         score_weighted = NA
       }
       
-      score <- data.frame(site=x["Site"],recodeScore = as.numeric(score_recoded), Weighted_Score = as.numeric(score_weighted), stringsAsFactors = FALSE)
+      score <-
+        data.frame(
+          site = x["Site"],
+          recodeScore = as.numeric(score_recoded),
+          Weighted_Score = as.numeric(score_weighted),
+          stringsAsFactors = FALSE
+        )
       
       return(score)
       
@@ -49,15 +139,33 @@ scorePhenotypeByLocation <-
     
     score <- bind_rows(score)
     score <- unique(score)
-    score <- score[,-1]
+    colnames(score) <- c("Site", "Unweighted", "Weighted")
     
-    colnames(score) <- c("Unweighted", "Weighted")
     
-    score_summary <- score %>%
-      summarise_all(.funs = c(sum = "sum",mean = "mean",sd = "sd"))
-
-   
+    score_category <- score
+    
+    
+    location_group_scores <- make_location_groupings(score_category)
+    
+    location_group_scores <- t(location_group_scores)
+    colnames(location_group_scores) <- location_group_scores[1, ]
+    
+    location_group_df <- as.data.frame(location_group_scores)
+    location_group_df <- location_group_df[c(2), ]
+    
+    score <- score[, -1]
+    
+    
+    
+    #### ADD CATEGORIZING FOR DIFFERENT DISEASE LOCATIONS ETC
+    
+    
+    score_summary <<- score %>%
+      summarise_all(.funs = c(sum = "sum", mean = "mean", sd = "sd"))
+    
+    
+    
+    score_summary <<- cbind(score_summary, location_group_df)
+    
     return(score_summary)
   }
-
-
